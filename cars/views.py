204c52b6e34fed4f75c83ca18c63082ed24e88fb
Thiago Karnopp <thiagokarnopp@MacBook-Air-de-Thiago.local>
2025-12-22 15:48:16 -0300

    feat: adiciona pagina de cadastro de carros

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarForm
 
 
 def cars_view(request):
@@ -16,3 +16,14 @@ def cars_view(request):
         {'cars': cars}
     )
 
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+
+    else:
+        new_car_form = CarForm()
+    return render(request, 'new_car.html', { 'new_car_form': new_car_form })
