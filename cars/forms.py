4d1e0f70414fd6956499c889b553ef1113ccec83
Thiago Karnopp <thiagokarnopp@MacBook-Air-de-Thiago.local>
2026-01-06 20:07:48 -0300

    Alteração do forms de cadastro de novos carros. Inclusão de campos de validação de dados para o ano de fabricação e valor mínimo para cadastro

--- cars/forms.py
+++ cars/forms.py
@@ -1,26 +1,20 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
-
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    factory_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
-    def save(self):
-        car = Car(
-            model = self.cleaned_data['model'],
-            brand = self.cleaned_data['brand'],
-            factory_year = self.cleaned_data['factory_year'],
-            model_year = self.cleaned_data['model_year'],
-            plate = self.cleaned_data['plate'],
-            value = self.cleaned_data['value'],
-            photo = self.cleaned_data['photo'],
-        )
-        car.save()
-        return car
           
+class CarModelForm(forms.ModelForm):    
+    class Meta:
+        model = Car
+        fields = '__all__'
+
+    def clean_value(self): # Regra para valor mínimo do carro cadastrado
+        value = self.cleaned_data.get('value')
+        if value < 5000:
+            self.add_error('value', 'Valor mínimo do carro deve ser R$ 5.000,00')
+        return value
+    
+    def clean_factory_year(self): # Regra para ano de fabricação mínimo para cadastrar carro
+        factory_year = self.cleaned_data.get('factor_year')
+        if factory_year < 1975:
+            self.add_error('factory_year', 'Não é possível cadastrar carros com ano de fabricação anterior a 1975')
+        return factory_year
